041990e5cf244ce8454335307124a9b9f4d03e34
Martin Falk <m.falkurt@gmail.com>
2016-06-29 13:42:20 +0200

    gooo

--- Website/js.js
+++ Website/js.js
@@ -6,7 +6,6 @@ hover(function() {
   $(this).not(".clickedTemp").css("opacity", "0.8");
 }).
 on("click", function(){
-  $("#email_adress").hide();
   $(this).addClass("clickedTemp");
   $(this).css("border-radius", "5px 5px 5px 5px");
 
@@ -16,32 +15,41 @@ on("click", function(){
     $.ajax({
       type: "POST",
       headers: {
-          'Accept': 'application/json',
+          'Accept': 'text/plain',
           'Content-Type': 'application/json'
       },
-      dataType: "json",
-      url: "http://stolktalktommy.nuui2tmhaj.eu-central-1.elasticbeanstalk.com/register",
+      url: "http://stolktalktommy.nuui2tmhaj.eu-central-1.elasticbeanstalk.com/inquiry",
       data: JSON.stringify({'email':email}),
       success: function(response){
-        alert(JSON.stringify(response));
+        console.log(JSON.stringify(response));
 
         if(response == "OK"){
-          $("#send_button").html("Tack!");
+          $("#send_button").html("Tack för visat intresse!");
+          $("#email_adress").hide();
         }
         else{
-          alert("Ett fel uppstod")
-          location.reload();
+          $("#h3").text("Ett fel uppstod, försök igen.");
+          setTimeout(function () {
+            reloader();
+          }, 1000);
+          }
         }
-      }
     });
   }
   else{
-    alert("Epost-adressen var inte giltig, försök igen.")
-    location.reload();
+    $("#h3").text("Ett fel uppstod, försök igen.");
+    setTimeout(function () {
+      reloader();
+    }, 1000);
   }
+
 });
 
 function isValidEmailAddress(emailAddress) {
     var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
     return pattern.test(emailAddress);
+};
+
+function reloader(){
+  location.reload();
 };
